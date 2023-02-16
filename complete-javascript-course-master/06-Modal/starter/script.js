9f4ec4f97d42b442239d043af77e9d3c83d21ac5
Tianhao Chen <tc30@illinois.edu>
2023-02-16 15:11:08 -0800

    Complete Modal Window exercise

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+
+const showModal = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+showModal.forEach(m => {
+  m.addEventListener('click', openModal);
+});
+
+// not closeModal()
+// because it will call the function as soon as the JavaScript loads
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
