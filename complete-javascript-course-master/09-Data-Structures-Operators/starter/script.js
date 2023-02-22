34f647ea582b0e708b9c9382dd629b3586d02301
Tianhao Chen <tc30@illinois.edu>
2023-02-21 21:28:15 -0600

    Complete DataStructures-Operators Part

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,339 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
 };
+
+// SPREAD
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient1?"),
+  // prompt('Ingredient2?'),
+  // prompt('Ingredient3?'),
+];
+console.log(restaurant.orderPasta(...ingredients));
+
+const newRestaurant = { foundedIn: 1970, ...restaurant, founder: 'James' };
+console.log(newRestaurant);
+
+const restaurantCopy = restaurant;
+restaurantCopy.name = 'Restaurant Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+// REST
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// REST Parameters
+const addRestPara = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+addRestPara(2, 3);
+addRestPara(2, 3, 4, 5, 6, 7, 8);
+const x = [23, 5, 7, 23];
+addRestPara(...x);
+
+// OR: return the first truthy value or the last value if all of the value are falsy
+console.log(0 || 'Henry'); // 'Henry'
+console.log(undefined || null); // null
+
+// AND: return the first falsy value or the last value if all of the value are truthy
+console.log(0 && 23 && null && 'Henry');
+
+// ??: Nullish Coalescing Operator
+// nullish -> null and undefined (not 0 or '')
+const restaurantGuests = 0;
+// 如果restaurantGuests是null or undefined的时候，才执行和return第二个
+const guestCorrect = restaurantGuests ?? 10;
+console.log(guestCorrect); // 0
+
+// looping arrays
+// for-of loop
+for (const item of menu) console.log(item);
+for (const item of menu.entries()) console.log(item);
+
+// Optional Chaining
+console.log(restaurant.openingHours?.mon?.open);
+console.log(restaurant.openingHours?.fri?.open);
+
+// String playground
+// padStart, padEnd
+const message = 'Go to Gate 23!';
+console.log(message.padStart(25, '+'));
+console.log(message.padEnd(25, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  console.log(last.padStart(str.length, '*'));
+};
+
+maskCreditCard(43685974589623132);
+maskCreditCard('2335668899774412');
+
+// Repeat
+const message2 = 'Bad Weather, all depatures delayed.';
+console.log(message2.repeat(5));
+
+// Coding Challenge #1
+/*
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game ('game' variable on
+next page). In this challenge we're gonna work with that data.
+
+Your tasks:
+1. Create one player array for each team (variables 'players1' and
+'players2')
+2. The first player in any player array is the goalkeeper and the others are field
+players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22
+players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+new array ('players1Final') containing all the original team1 players plus
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
+operator.
+
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+Then, call the function again with players from game.scored
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const { team1, x: draw, team2 } = game.odds;
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored.`);
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+team1 < team2 && console.log('Team1 will win!');
+team1 > team2 && console.log('Team2 will win!');
+
+/*
+Coding Challenge #2
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+  Odd of victory Bayern Munich: 1.33
+  Odd of draw: 3.25
+  Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names 😉
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+Gnarby: 1,
+Hummels: 1,
+Lewandowski: 2
+}
+*/
+// #1
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+// #2
+let avg = 0;
+for (const odd of Object.values(game.odds)) {
+  avg += odd;
+}
+avg /= Object.values(game.odds).length;
+console.log(avg);
+// #3
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamString = team === 'x' ? 'draw' : `${game[team]}`;
+  console.log(`Odd of victory ${teamString}: ${odd}`);
+}
+// #4
+let scorers = {};
+for (const player of game.scored.values()) {
+  let tempCounter = 1;
+  if (scorers[player]) {
+    tempCounter += 1;
+  }
+  scorers[player] = tempCounter;
+}
+console.log(scorers);
+
+/*
+Coding Challenge #3
+
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+*/
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+gameEvents.delete(64);
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const [minutes, events] of gameEvents) {
+  const halfStr = minutes < 45 ? 'FIRST HALF' : 'SECOND HALF';
+  console.log(`[${halfStr}] ${minutes}: ${events}`);
+}
+
+/*
+Coding Challenge #4
+
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+underscoreCase   ✅
+firstName        ✅✅
+someVariable     ✅✅✅
+calculateAge     ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+
+Hints:
+§ Remember which character defines a new line in the textarea 😉
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click', function () {
+  let text = document.querySelector('textarea').value;
+  const strText = text.split('\n');
+  for (const [i, str] of strText.entries()) {
+    const [first, second] = str.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
+// Coding Challenge #4
+const flightsInfo =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:4+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flightsInfo.split('+')) {
+  const [status, departure, arrival, time] = flight.split(';');
+
+  console.log(
+    `${status.startsWith('_Delayed') ? '🔴' : ''}${status.replaceAll(
+      '_',
+      ' '
+    )} from ${getCode(departure)} to ${getCode(arrival)} (${time.replace(
+      ':',
+      'h'
+    )})`.padStart(45)
+  );
+}
